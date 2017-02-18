1164fbff83aecfa7e35a4a2f96fbc24d1ce3b92f
wladi0 <lena.epvp@web.de>
2017-02-19 00:14:15 +0100

    .
    .

---
+++ MotivationBuddy/Menu.cs
@@ -0,0 +1,29 @@
+﻿using EloBuddy;
+using EloBuddy.SDK.Menu;
+using EloBuddy.SDK.Menu.Values;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MotivationBuddy
+{ 
+    internal class Menus
+    {
+        public static Menu FirstMenu;
+
+
+
+        public static void CreateMenu()
+        {
+
+            FirstMenu = MainMenu.AddMenu("Motivation " + "Buddy", Player.Instance.ChampionName.ToLower() + "Buddy");
+
+            FirstMenu.AddGroupLabel("Settings");
+            FirstMenu.Add("EnableM", new CheckBox("- Enable motivation"));
+            FirstMenu.Add("EnableT", new CheckBox("- Enable Tilt"));
+            FirstMenu.Add("Delay", new Slider("- Delay Slider", 100, 0, 10000));
+        }
+    }
+}
