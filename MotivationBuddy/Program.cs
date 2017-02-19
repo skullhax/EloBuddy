53818c3077c2e1e29a49e03bff6413b2d192214b
wladi0 <lena.epvp@web.de>
2017-02-19 02:37:59 +0100

    .
    .

--- MotivationBuddy/Program.cs
+++ MotivationBuddy/Program.cs
@@ -62,23 +62,31 @@ namespace MotivationBuddy
                     case GameEventId.OnChampionKill:
                         if ((Sender == AllyK.NetworkId || Sender == AllyD.NetworkId ) && Sender != myhero.NetworkId)
                         {
-                            string[] Motivation1 = { "Good Job!", "Nice man", "really nice", "well done", "well played" };
+                            string[] Motivation1 = { "Good Job!", "Nice man", "really nice", "well done", "well played", "gj", "wp", "gj wp", "well done", "well done mate", "nice", "nice play", "You did good there", "let's push", "good job", "nice work", "good play there bro", "we are going to win this", "we will win" };
 
                             Random RandName = new Random();
                             string Temp1 = Motivation1[RandName.Next(0, Motivation1.Length)];
 
                             Core.DelayAction(() => Chat.Say(Temp1), FirstMenu["Delay"].Cast<Slider>().CurrentValue);
+                            Core.DelayAction(() => Chat.Say("/Masterybadge"), FirstMenu["Delay"].Cast<Slider>().CurrentValue);
+                        }
+                        if (Sender == myhero.NetworkId)
+                        {
+                            Core.DelayAction(() => Chat.Say("/Masterybadge"), FirstMenu["Delay"].Cast<Slider>().CurrentValue);
+                            Player.DoEmote(Emote.Laugh);
+
                         }
                         break;
                     case GameEventId.OnChampionDie:
                         if ((Sender == AllyD.NetworkId || Sender == AllyK.NetworkId) && Sender != myhero.NetworkId)
                         {
-                            string[] Motivation2 = { "Next time you get him!", "Nice try, next time maybe", "Don't get greedy", "Be less agressive", "Don't lose motivation", "Don't give up" };
+                            string[] Motivation2 = { "Next time you get him!", "Nice try, next time maybe", "Don't get greedy", "Be less agressive", "Don't lose motivation", "Don't give up", "bad luck", "come on let's team fight" };
 
                             Random RandName = new Random();
                             string Temp2 = Motivation2[RandName.Next(0, Motivation2.Length)];
 
                             Core.DelayAction(() => Chat.Say(Temp2), FirstMenu["Delay"].Cast<Slider>().CurrentValue);
+                            Core.DelayAction(() => Chat.Say("/Masterybadge"), FirstMenu["Delay"].Cast<Slider>().CurrentValue);
                         }
                         break;
                 }
@@ -99,12 +107,14 @@ namespace MotivationBuddy
                     case GameEventId.OnChampionDie:
                         if (Sender == Enemy.NetworkId || Sender == EnemyD.NetworkId || Sender == EnemyDD.NetworkId || Sender == EnemyDDD.NetworkId || Sender != myhero.NetworkId)
                         {
-                            string[] Tilt2 = { "/all You're bad", "/all You suck", "/all Nice try", "/all Go play against bots", "/all noob", "/all ez", "/All so bad", "/all learn 2 play", "/all hahahha", "/all bad", "/All rekt", "/All boosted" };
+                            string[] Tilt2 = { "/all You're bad", "/all You suck", "/all Nice try", "/all Go play against bots", "/all noob", "/all ez", "/All so bad", "/all learn 2 play", "/all hahahha", "/all bad", "/All rekt", "/All boosted", "/all wood V", "/all bronze V", "/all What is this elo", "/all xd", "/all so ez", "/all l2p","/all boring","/all salt", "/all tilt", "/all so bad lmao", "/all are you trolling or just bad?" };
 
                             Random RandName = new Random();
                             string Temp2 = Tilt2[RandName.Next(0, Tilt2.Length)];
 
                             Core.DelayAction(() => Chat.Say(Temp2), FirstMenu["Delay"].Cast<Slider>().CurrentValue);
+                            Player.DoEmote(Emote.Laugh);
+                            Core.DelayAction(() => Chat.Say("/Masterybadge"), FirstMenu["Delay"].Cast<Slider>().CurrentValue);
                         }
                         break;
                 }
